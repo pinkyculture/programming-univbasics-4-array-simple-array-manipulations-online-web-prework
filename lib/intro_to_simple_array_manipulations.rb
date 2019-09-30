7e222b768c9deed69d2ad834b19f088ae051ef6b
interactive-cache-8375 <randomblue1@yahoo.com>
2019-09-30 20:51:19 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,17 @@
+def using_push (colors_in_the_rainbow , next_color)
+  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+  next_color = "violet"
+  colors_in_the_rainbow.push(next_color)
+end
+  
+def using_unshift (bouroughs_in_nyc , new_neighborhood)
+bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+new_neighborhood = "Staten Island"
+bouroughs_in_nyc.unshift(new_neighborhood)
+end
+
+def using_pop (array)
+  array = ["nothing" , "here"]
+  array.pop
+  
+  end 
